22c27ac3c8e27b000fe93776b5bfa95654d84942
Jesus <jesuscarrascosa8@gmail.com>
2023-11-15 15:21:50 +0800

    funciona el streaming de video

--- app/src/main/java/com/nuwarobotics/sample/camera/FrameStreamingActivity.java
+++ app/src/main/java/com/nuwarobotics/sample/camera/FrameStreamingActivity.java
@@ -4,13 +4,12 @@ import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
 
 import androidx.appcompat.app.AppCompatActivity;
-
-import com.codertainment.dpadview.DPadView;
 
 import java.io.IOException;
 import java.io.InputStream;
@@ -19,8 +18,7 @@ import java.io.OutputStream;
 import java.net.InetAddress;
 import java.net.Socket;
 
-
-public class FrameStreamingActivity extends AppCompatActivity {
+public class FrameStreamingActivity extends AppCompatActivity implements View.OnClickListener {
 
     /*
      * Available resolutions for NB2 are:
@@ -55,12 +53,15 @@ public class FrameStreamingActivity extends AppCompatActivity {
     private EditText etPort;
     private InputStream input;
     private OutputStream output;
-    // private Button btnSet;//unnecesary
+
     private Button btnConnect;
     private String serverIP;
     Thread bitmapThread;
-    private  static DPadView gamepad;
-    //TODO the gamepad button
+
+
+
+    //TODO the gamepad but
+    // ton
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -71,118 +72,106 @@ public class FrameStreamingActivity extends AppCompatActivity {
         etPort  = findViewById(R.id.portText);
         //btnSet = findViewById(R.id.setIp);//innecesario
         btnConnect = findViewById(R.id.btnConnect);
-       // gamepad = findViewById(R.id.dpad);
-
-
-        //btnConnect.setOnClickListener();
-        initView();
+        btnConnect.setOnClickListener(this);
+        mImageFrame = findViewById(R.id.img_frame);
+        findViewById(R.id.upButton).setOnClickListener(this);
+        findViewById(R.id.downButton).setOnClickListener(this);
+        findViewById(R.id.leftButton).setOnClickListener(this);
+        findViewById(R.id.rightButton).setOnClickListener(this);
+        findViewById(R.id.onButton).setOnClickListener(this);
+        findViewById(R.id.offButton).setOnClickListener(this);
     }
 
     @Override
     protected void onDestroy() {
         try {
-            client.close();
+            if (null != client && !client.isClosed())
+                client.close();
         } catch (IOException e) {
             e.printStackTrace();
         }
         super.onDestroy();
     }
 
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()){
+            case R.id.downButton:
+
+                break;
+            case R.id.upButton:
+
+                break;
+            case R.id.leftButton:
+                break;
+            case R.id.rightButton:
+                break;
+            case R.id.onButton:
+                turnOnStreaming();
+                break;
+            case R.id.offButton:
+                break;
+            case R.id.btnConnect:
+                new Thread(() -> {
+                    serverIP = etIP.getText().toString().trim();
+                    remotePortNumber = Integer.parseInt((etPort.getText().toString().trim()));
+
+                    Log.d("jesus", "retireved server ip");
+                    // serverIP = "192.168.85.130";
+                    // we connect to the socket
+                    // Avoid duplication connections
+                    try {
+                        if(client != null && !client.isClosed()) {
+                            client.close();
+                        }
+                    } catch (IOException e) {
+                        //e.printStackTrace();
+                        Log.w("jesus", "e.getMessage() = " + e.getMessage());
+                    }
+
+                    try {
+                        client = new Socket(InetAddress.getByName(serverIP), remotePortNumber);
+                        Log.d("socket","client " + client + " try to connect " + serverIP + ":" + remotePortNumber);
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+
+                    // Just for verification
+
+                }).start();
+                break;
+        }
+    }
+
+    private void turnOnStreaming() {
+    new Thread(()->{
+        if (null != client) {
+            while (!client.isClosed()) {
+                if (client.isConnected()) {
+                    Log.i("socket", "socket connected");
+                    try {
+                        ObjectInputStream ois = new ObjectInputStream(client.getInputStream());
+                        byte[] bytes = (byte[]) ois.readObject();
+
+                        Bitmap bmp = BitmapFactory.decodeByteArray(bytes, 0, bytes.length);
+//                        Bitmap bmp = BitmapFactory.decodeStream(client.getInputStream());
+                        Log.i("bmp", "bitmap decoded");
+                        runOnUiThread(() -> mImageFrame.setImageBitmap(bmp));
+                    } catch (Exception e) {
+                        Log.w("jesus", "parse bmp exception message = " + e.getMessage());
+                    }
+                }
+            }
+        }
+    }).start();
+    }
+
     private enum CONNECTION_STATE {
         DISCONNECTED, CONNECTING, CONNECTED
     }
 
-    private void initView() {
-
-
-        btnConnect.setOnClickListener((v) -> {
-            // first we get ip
-            new Thread(() -> {
-               serverIP = etIP.getText().toString().trim();
-               remotePortNumber = Integer.parseInt((etPort.getText().toString().trim()));
-
-                Log.d("jesus", "retireved server ip");
-               // serverIP = "192.168.85.130";
-                // we connect to the socket
-                try {
-                    client = new Socket(InetAddress.getByName(serverIP), remotePortNumber);
-                    Log.d("socket","client connected");
-                }catch (IOException e) {
-                   //e.printStackTrace();
-                    Log.w("jesus",e.getMessage());
-                }
-
-                //we start the receive bitmap
-               // receiveBitmap();
-                receiveTest();
-            }).start();
-
-            //
-        });
-
-
-
-    }
-
-    private void receiveBitmap() {
-        while (client.isConnected()) {
-            try {
-                input = client.getInputStream();
-                //TODO deermine buffer size i thinhk is height withd * 4
-                //or *3
-                int bytestoStore = WIDTH * HEIGHT * 4;
-                byte[] buffer = new byte[bytestoStore]; //
-                int bytesRead;
-                // TODO How we decode the array into a bitmap
-				/*
-			byte[] buffer = new byte[bitmapSize];
-            int bytesRead;
-            ByteArrayOutputStream bitmapBuffer = new ByteArrayOutputStream();
-
-            while ((bytesRead = inputStream.read(buffer)) != -1) {
-                bitmapBuffer.write(buffer, 0, bytesRead);
-            }*/
-
-                // i think it could be like this
-                input.read(buffer);
-                Bitmap bmp = BitmapFactory.decodeByteArray(buffer, 0, bytestoStore);
-                mImageFrame.setImageBitmap(bmp);
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-        }
-    }
-
-    //   });
-private void receiveTest(){
-
-while (null != client){
-    if(client.isConnected()){
-        try {
-            ObjectInputStream ois = new ObjectInputStream(input);
-            byte[] bytes = (byte[])ois.readObject();
-            Bitmap  bmp = BitmapFactory.decodeByteArray(bytes,0,bytes.length);
-            runOnUiThread(()-> ((ImageView)findViewById(R.id.img_frame)).setImageBitmap(bmp));
-
-        }catch (Exception e){
-            Log.w("Jesus","parse bmp exception message" +e.getMessage());
-        }
-
-    }
-}
-}
-    //TODO Send command
+    // TODO: Send command
     private  void sendCommand(){
 
-
-
-
-
     }
-
-
-
-    }
-
-
-
+}
